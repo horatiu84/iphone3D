d9ca9ef6ec27960cb02826cec2815c64996b44d5
horatiu84 <horatiul@yahoo.com>
2023-03-10 06:42:06 +0200

    finishig desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,26 @@ import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
 import DisplaySection from "./components/DisplaySection";
 import WebgiViewer from "./components/WebgiViewer";
+import { useRef } from "react";
 
 function App() {
 
+  const webgiViewerRef = useRef();
+  const contentRef = useRef()
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  }
+
   return (
     <div className="App">
+    <div ref={contentRef} id="content">
       <Nav />
       <Jumbotron />
       <SoundSection />
-      <DisplaySection />
-      <WebgiViewer />
+      <DisplaySection triggerPreview={handlePreview}/>
+    </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const DisplaySection = () => {
+const DisplaySection = ({ triggerPreview }) => {
+  const handleScrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className='display-section wrapper'>
-        <h2 className='title'>New</h2>
-        <p className='text'>Brilliant.</p>
-        <span className='description'>
-          A display that's up 2x brighter in the sun
-        </span>
-        <button className='button'>Try me!</button>
-        <button className='back-button'>TOP</button>
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant.</p>
+      <span className="description">
+        A display that's up 2x brighter in the sun
+      </span>
+      <button className="button" onClick={triggerPreview}>
+        Try me!
+      </button>
+      <button className="back-button" onClick={handleScrollUp}>
+        TOP
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default DisplaySection
+export default DisplaySection;
